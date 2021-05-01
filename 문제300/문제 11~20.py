99bd8e5e16345965118e3999c01828704f472665
Seo-Hongju <tjghdwn0824@naver.com>
2021-05-01 15:55:48 +0900

    파이썬 문제

---
+++ 문제300/문제 11~20.py
@@ -0,0 +1,49 @@
+# 변수
+
+# 011.
+삼성전자 = 50000
+print(삼성전자*10)
+
+# 012.
+시가총액 = 298000000000000
+현재가 = 50000
+PER = 15.79
+
+# 013.
+s = "hello"
+t = "python"
+print(s+"!",t)
+
+# 014.
+# 예상 결과
+# 8
+
+# 015.
+a = 132
+print(type(a))
+
+# 016.
+num_str = '720'
+print(int(num_str))
+
+# 017.
+num = 100
+print(str(num))
+
+# 018.
+문자열 = "15.79"
+print(float(문자열))
+
+# 019.
+year = "2020"
+print(int(year))
+year = 2019
+print(year)
+year = 2018
+print(year)
+year = 2017
+print(year)
+
+# 020.
+가격 = 48584
+print(가격*36)
--- 문제300/문제1~10.py
+++ 문제300/문제1~10.py
@@ -1,2 +1,33 @@
+# 출력하기
 
-# 문제 1
+# 001.
+print("Holle World")
+
+# 002.
+print("Mary's cosmetics")
+
+# 003.
+print('신씨가 소리질렸다. "도둑이야".')
+
+# 004.
+print("C\Windows")
+
+# 005.
+# \n : 줄바꿈
+# \t : 들여쓰기
+
+# 006.
+# 예상결과
+# 오늘은 일요일
+
+# 007.
+print("naver","kakao","sk","samsung",sep=";")
+
+# 008.
+print("naver","kakao","sk","samsung",sep="/")
+
+# 009.
+print("first",end="");print("second")
+
+# 010.
+print(5/3)
