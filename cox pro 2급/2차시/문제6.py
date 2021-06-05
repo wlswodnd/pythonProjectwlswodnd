47034610858447cae9d35142eaf47c6f0e2247ec
Byungwoo Kim <rlaquddn05@naver.com>
2021-06-05 17:15:21 +0900

    파이썬 문제

--- cox pro 2급/2차시/문제6.py
+++ cox pro 2급/2차시/문제6.py
@@ -3,11 +3,11 @@
 # 문제 6
 def solution(floors):
     dist = 0
-    length = len(floors)
-    for i in range(length):
-        if i == floors[i+1] :
+    length = len(floors)    # 리스트내 층 개수 : 5
+    for i in range(1,length): # 1~5 전까지
+        if floors[i] > floors[i-1] :    # 현재층이 앞전층보다 크다면
             dist += floors[i] - floors[i-1]
-        else:
+        else:   # 아니면
             dist += floors[i-1] - floors[i]
     return dist
 
