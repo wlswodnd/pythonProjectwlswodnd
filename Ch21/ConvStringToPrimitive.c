707028c61517e85832bd1f343a2b978cda871102
wlswodnd <83488101+wlswodnd@users.noreply.github.com>
2021-07-19 13:31:51 +0900

    Add files via upload

---
+++ Ch21/ConvStringToPrimitive.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning (disable:4996)
+
+int main(void)
+{
+	char str[20];
+	printf("정수 입력: ");
+	scanf("%s", str);
+	printf("%d \n", atoi(str));
+
+	printf("실수 입력: ");
+	scanf("%s", str);
+	printf("%g \n", atof(str));
+	return 0;
+}
